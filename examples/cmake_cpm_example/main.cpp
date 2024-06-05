46db2f7bc794349ade49e6dcb807fff450b6fe8a
Alexis Paques <alexis.paques@gmail.com>
2024-06-05 11:32:02 +0200

    Add cmake usage of the library using CPM.

---
+++ examples/cmake_cpm_example/main.cpp
@@ -0,0 +1,20 @@
+#include <flacon/flacon.hpp>
+
+int main(int argc, char** argv) {
+  flacon::File config;
+  bool success = config.init("config.json", "defaults.json");
+  if (!success) {
+    fprintf(stderr, "We failed to open, read or write the configuration files.");
+    exit(EXIT_FAILURE);
+  }
+
+  int int_value = config.get("/int", 1);
+  auto some_string = config.get<std::string>("/oh/hi", "mark");
+  auto a_vector = config.get<std::vector<float>>("/vector", {1., 2., 3.});
+
+  int_value = config.set("/int", 42);
+  if(config.should_write()) { // Only write it if there is a change
+    config.write();
+  }
+  exit(EXIT_SUCCESS);
+}
