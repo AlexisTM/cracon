147a690b78f4bd0c906e2da3de7c7ac28ff6e9df
Alexis Paques <alexis.paques@gmail.com>
2024-06-05 16:09:18 +0200

    Init data, just in case.

--- include/cracon/cracon.hpp
+++ include/cracon/cracon.hpp
@@ -169,7 +169,7 @@ class SharedFile : public File {
      * @brief Allow to store this structure on the stack to avoid double
      * indirection.
      */
-    Param(): data({}) {};
+    Param(): data_({}) {};
 
     /**
      * @brief Fetches a copy of the current data. This doesn't touch the file.
